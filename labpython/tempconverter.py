32660e4133f998c82247b44d5eff2e149fabdcdf
Ubuntu <DJWilliams22@DwillVM.wgzebgxoikxepm2ibzfa5wtsed.gx.internal.cloudapp.net>
2020-04-02 23:07:11 +0000

    Celcius to Farenheit converter

---
+++ labpython/tempconverter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+Celsius = int(raw_input("Enter a temperature in Celsius: "))
+ 
+Fahrenheit = 9.0/5.0 * Celsius + 32
+ 
+print "Temperature:", Celsius, "Celsius = ", Fahrenheit, " F"
+
